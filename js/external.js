f4a1bcf3e14c2b2370b7c5e5520ac9bec50ab444
andrewswint <andrewswint1976@gmail.com>
2019-10-05 10:58:22 -0500

    work in progress

--- js/external.js
+++ js/external.js
@@ -1,8 +1,17 @@
 "use strict";
+//
+// console.log("Hello from external JavaScript");
+//
+// alert("Welcome to my Website!");
+//
+// prompt("What is your favorite color?");
+// alert("That's great, my favorite color is green!");
 
-console.log("Hello from external JavaScript");
+var little = prompt("How long would you rent The Little Mermaid for $3 a day?");
+parseInt(little);
+var bear = prompt("How long would you rent Brother Bear for $3 a day?");
+parseInt(bear);
+var herc = prompt("How long would you rent Hercules for $3 a day?");
+parseInt(herc);
 
-alert("Welcome to my Website!");
-
-prompt("What is your favorite color?");
-alert("That's great, my favorite color is green!");
+console.log("Ok, you will spend " + (little + bear + herc)*3);
