0fd015a0d89f254f21886f9cbbfa4134ac6d1a20
andrewswint <andrewswint1976@gmail.com>
2019-10-05 11:32:47 -0500

    problem 1 done

--- js/external.js
+++ js/external.js
@@ -7,11 +7,15 @@
 // prompt("What is your favorite color?");
 // alert("That's great, my favorite color is green!");
 
-var little = prompt("How long would you rent The Little Mermaid for $3 a day?");
-parseInt(little);
-var bear = prompt("How long would you rent Brother Bear for $3 a day?");
-parseInt(bear);
-var herc = prompt("How long would you rent Hercules for $3 a day?");
-parseInt(herc);
 
-console.log("Ok, you will spend " + (little + bear + herc)*3);
+//---------problem 1---------------
+var little = prompt("How long would you rent The Little Mermaid for $3 a day?");
+little = parseInt(little);
+var bear = prompt("How long would you rent Brother Bear for $3 a day?");
+bear = parseInt(bear);
+var herc = prompt("How long would you rent Hercules for $3 a day?");
+herc = parseInt(herc);
+
+alert("Ok, you will spend " + (little + bear + herc) * 3);
+
+//--------------problem 2------------
