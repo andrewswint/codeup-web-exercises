5ebbf2592818ab1d12e01fb082020129467e4d81
andrewswint <andrewswint1976@gmail.com>
2019-10-05 10:10:42 -0500

    finished part of the exercise

--- js/external.js
+++ js/external.js
@@ -1,3 +1,8 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+prompt("What is your favorite color?");
+alert("That's great, my favorite color is green!");
