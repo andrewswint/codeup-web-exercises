80825c1b750a3bc2b37d01cd670378a2982bffd2
andrewswint <andrewswint1976@gmail.com>
2019-10-07 14:01:04 -0500

    Trying to call functions

--- js/external.js
+++ js/external.js
@@ -1,75 +1,77 @@
 "use strict";
-//
-// console.log("Hello from external JavaScript");
-//
-// alert("Welcome to my Website!");
-//
-// prompt("What is your favorite color?");
-// alert("That's great, my favorite color is green!");
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+prompt("What is your favorite color?");
+alert("That's great, my favorite color is green!");
 
 
 //---------problem 1---------------
 //You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
 
 
-// var little = prompt("How long would you rent The Little Mermaid for $3 a day?");
-// little = parseInt(little);
-//
-// var bear = prompt("How long would you rent Brother Bear for $3 a day?");
-// bear = parseInt(bear);
-//
-// var herc = prompt("How long would you rent Hercules for $3 a day?");
-// herc = parseInt(herc);
-//
-// alert("Ok, you will spend $" + (little + bear + herc) * 3);
+var little = prompt("How long would you rent The Little Mermaid for $3 a day?");
+little = parseInt(little);
+
+var bear = prompt("How long would you rent Brother Bear for $3 a day?");
+bear = parseInt(bear);
+
+var herc = prompt("How long would you rent Hercules for $3 a day?");
+herc = parseInt(herc);
+
+alert("Ok, you will spend $" + (little + bear + herc) * 3);
+
 
 // //--------------problem 2------------
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 
-// var google = prompt("How many hours did you work at Google this week?");
-// google = parseInt(google) * 400;
-//
-// var amazon = prompt("How many hours did you work at Amazon this week?");
-// amazon = parseInt(amazon) * 380;
-//
-// var facebook = prompt("How many hours did you work at Facebook this week?");
-// facebook = parseInt(facebook) * 350;
-//
-// alert("Great job! You made $" + (facebook + amazon + google) + "!");
+var google = prompt("How many hours did you work at Google this week?");
+google = parseInt(google) * 400;
+
+var amazon = prompt("How many hours did you work at Amazon this week?");
+amazon = parseInt(amazon) * 380;
+
+var facebook = prompt("How many hours did you work at Facebook this week?");
+facebook = parseInt(facebook) * 350;
+
+alert("Great job! You made $" + (facebook + amazon + google) + "!");
+
 
 // -------------problem 3-----------------
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
 
-// var classFull = confirm ("Is your class full?");
-// var scheduleConflict = confirm("Is there a schedule conflict?");
-// var enroll= !scheduleConflict && !classFull;
-//
-// function prob3response (){
-//     if (enroll === true) {
-//         alert("Congratulations! You may enroll!");
-//     } else {
-//         alert("Sorry, you may not enroll.");
-//     }
-// }
-// prob3response();
+var classFull = confirm ("Is your class full?");
+var scheduleConflict = confirm("Is there a schedule conflict?");
+var enroll= !scheduleConflict && !classFull;
+
+function prob3response (){
+    if (enroll === true) {
+        alert("Congratulations! You may enroll!");
+    } else {
+        alert("Sorry, you may not enroll.");
+    }
+}
+prob3response();
+
 
 //------------problem 4----------------------
 // A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
 
-// var cart = parseInt(prompt("How many items in your cart?"));
-// var valid = confirm("Is the offer valid?");
-// var premium = confirm("Are you a premium member?");
-// var applies = (cart > 2 || premium === true) && valid === true;
-//
-// function prob4response() {
-//     if (applies === true) {
-//         alert("Your offer applies.")
-//     } else {
-//         alert("Sorry, the offer is not valid.")
-//     }
-// }
-// prob4response();
+var cart = parseInt(prompt("How many items in your cart?"));
+var valid = confirm("Is the offer valid?");
+var premium = confirm("Are you a premium member?");
+var applies = (cart > 2 || premium === true) && valid === true;
 
+function prob4response() {
+    if (applies === true) {
+        alert("Your offer applies.")
+    } else {
+        alert("Sorry, the offer is not valid.")
+    }
+}
+prob4response();
 
 
 //-------------problem 5--------------------
@@ -79,6 +81,7 @@
 // the password must not include the username
 // the username must be no more than 20 characters
 // neither the username or password can start or end with whitespace
+
 do {
     var username = prompt("Please enter a username.");
     var password = prompt("Please enter a password?");
