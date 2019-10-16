88e117c8c8296451c5eb731a1d1793f76910f17b
andrewswint <andrewswint1976@gmail.com>
2019-10-16 10:08:16 -0500

    ug

---
+++ js/break_and_continue.js
@@ -0,0 +1,10 @@
+// do {
+//     var x = prompt("enter an odd number between 1-50");
+// } while (x % 2 === 0);
+//
+// for (var x = 0; x <= 50; x++) {
+//      x = prompt("enter an odd number between 1-50");
+//     var y = (x % 2 !== 0);
+//         console.log(y + " is an odd number")
+//     }
+
---
+++ js/for_loops.js
@@ -0,0 +1,25 @@
+// function showMultiplicationTable (number) {
+//     for (var x = 1; x <= 10; x++) {
+//         console.log(x * number);
+//     }
+//
+// }
+
+
+
+// for (var x = 0; x < 10; x++) {
+//     var random = Math.floor(Math.random() * 20) + 100;
+//     if (random % 2 === 0) {
+//         console.log( random + " is even")
+//     } else {
+//         console.log(random + " is odd")
+//     }
+// }
+
+for (var x = 1; x <=9; x++) {
+    console.log("" + x.repeat(x));
+}
+
+// for (var x = 100; x > 0; x-=5) {
+//     console.log(x);
+// }
