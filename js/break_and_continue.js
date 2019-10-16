8f8a6e5de353b50c33b33d946c912acb28ae9ae2
andrewswint <andrewswint1976@gmail.com>
2019-10-16 14:03:54 -0500

    exercise walk through

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,23 +1,25 @@
-// do {
-//     var x = prompt("enter a odd number between 1-50");
-// } while (x % 2 === 0);
-//
 // var y;
 // for (var x = 0; x <= 50; x++) {
 //     y = parseFloat(prompt("enter a odd number between 1-50"));
-//     if (y % 2 !== 0) {
+//     if (y % 2 !== 0 && y >0) {
 //         console.log("your number is " + y);
 //         break;
 //     }
 // }
-//
-//
-// for (var a = 1; a <= 50; a+=2) {
-//     if (a === y) {
-//         console.log("yikes your number was " + a);
-//         continue;
-//     }
-//     console.log(a + " is odd");
-// }
+
+while (true) {
+    var y = parseFloat(prompt("enter a odd number between 1-50"));
+    if (y % 2 !== 0 && y >0) {
+        console.log("your number is " + y);
+        break;
+    }
+}
+for (var a = 1; a <= 50; a+=2) {
+    if (a === y) {
+        console.log("yikes your number was " + a);
+        continue;
+    }
+    console.log(a + " is odd");
+}
 
 
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,13 +1,13 @@
-function showMultiplicationTable (number) {
-    for (var x = 1; x <= 10; x++) {
-        console.log(x  + " * " + number + " = " + (number * x));
-    }
-}
-showMultiplicationTable(5);
+// function showMultiplicationTable (number) {
+//     for (var x = 1; x <= 10; x++) {
+//         console.log(x  + " * " + number + " = " + (number * x));
+//     }
+// }
+// showMultiplicationTable(5);
 
 
 // for (var x = 0; x < 10; x++) {
-//     var random = Math.floor(Math.random() * 20) + 100;
+//     var random = Math.floor(Math.random() * 180) + 20;
 //     if (random % 2 === 0) {
 //         console.log( random + " is even")
 //     } else {
