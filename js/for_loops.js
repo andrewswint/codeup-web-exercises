7eacbd5fabb5520d7c316904725a630f1950cc29
andrewswint <andrewswint1976@gmail.com>
2019-10-16 12:30:38 -0500

    walk through

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,9 @@
-// function showMultiplicationTable (number) {
-//     for (var x = 1; x <= 10; x++) {
-//         console.log(x * number);
-//     }
-//
-// }
-
+function showMultiplicationTable (number) {
+    for (var x = 1; x <= 10; x++) {
+        console.log(x  + " * " + number + " = "  (number * x));
+    }
+}
+showMultiplicationTable(5);
 
 
 // for (var x = 0; x < 10; x++) {
--- js/while.js
+++ js/while.js
@@ -1,26 +1,30 @@
-//problem 2
+// problem 2
+
 // var i = 1;
-// while (i < 65535) {
-//     console.log(i * 2);
-//     i*=2;
+// while (i < 65536) {
+//     console.log(i *= 2);
 // }
 
 
 //problem 3
+
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
 
-
+//
 // do {
 //     var conesWanted = Math.floor(Math.random() * 5) + 1;
-
-//     if(conesWanted <= allCones){
+//
+//     if(allCones > conesWanted){
 //         allCones -= conesWanted;
 //         console.log("I just sold " + conesWanted);
-//     }else{
+//     }else if (allCones < conesWanted) {
 //         console.log("I don't have " + conesWanted);
+//     } else {
+//         allCones -= conesWanted;
+//         console.log("yay im out");
 //     }
 // } while (allCones > 0);
-// console.log("yay");
+
 
