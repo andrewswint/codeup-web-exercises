e804e4712a0b3de54225d00103a883b76995d607
andrewswint <andrewswint1976@gmail.com>
2019-10-21 12:30:54 -0500

    lunch

---
+++ js/objects-bonus.js
@@ -0,0 +1,88 @@
+function makeCar (make, model) {
+    var car = {
+        make: make,
+        model: model
+    }
+    return car;
+}
+
+// console.log(makeCar("subaru", "outback"));
+
+var salesReport = {
+  title: "Monthly Sales Report",
+  date: "03-17-2015",
+  office: "Codeup",
+  employees: [
+      {
+          employeeNumber: 1,
+          firstName: "Jane",
+          lastName: "Janeway",
+          salesUnits: 3
+      },
+      {
+          employeeNumber: 3,
+          firstName: "Tricia",
+          lastName: "Triciason",
+          salesUnits: 5
+      },
+      {
+          employeeNumber: 4,
+          firstName: "Jeannette",
+          lastName: "Jeanson",
+          salesUnits: 4
+      },
+      {
+          employeeNumber: 5,
+          firstName: "Charles Emmerson III",
+          lastName: "Winchester",
+          salesUnits: 2
+      },
+      {
+          employeeNumber: 6,
+          firstName: "Chet",
+          lastName: "Chedderson",
+          salesUnits: 12
+      },
+      {
+          employeeNumber: 7,
+          firstName: "Chaiam",
+          lastName: "Chaiamson",
+          salesUnits: 8
+      },
+      {
+          employeeNumber: 8,
+          firstName: "Dale",
+          lastName: "Dalesinger",
+          salesUnits: 1
+      },
+      {
+          employeeNumber: 9,
+          firstName: "Zig",
+          lastName: "Ziglar",
+          salesUnits: 50
+      },
+      {
+          employeeNumber: 10,
+          firstName: "Henry",
+          lastName: "Kissinger",
+          salesUnits: 1
+      },
+      {
+          employeeNumber: 11,
+          firstName: "Arthur Herbert",
+          lastName: "Fonarelli",
+          salesUnits: 23
+      },
+      {
+          employeeNumber: 12,
+          firstName: "Betty",
+          lastName: "Boop",
+          salesUnits: 67
+      }
+  ]
+};
+
+var getEmployeeCount = function (){
+
+};
+
