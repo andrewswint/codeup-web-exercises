a78b5d2e593766f758cabcff1935d8954f0c88f9
andrewswint <andrewswint1976@gmail.com>
2019-11-03 17:45:42 -0600

    trying to style a little

--- js/numberGuessing.js
+++ js/numberGuessing.js
@@ -1,11 +1,3 @@
-var bucket = 0;
-$(document).ready(function() {
-    $(function(){
-        $("#countstroke").keyup(function(){
-            $("#count").text("Count: " + (++bucket));
-        });
-    });
-});
 
 //------------random number generator----------------------
 function randomNumber(min, max) {
