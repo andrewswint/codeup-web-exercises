db964f06d970c7a47b812793ea50601b17ec4f70
andrewswint <andrewswint1976@gmail.com>
2019-11-06 10:02:18 -0600

    icons

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    console.log("Hello");
     //-------------------------darkSky code-----------------------------------
     //-------------icons-----------------
 
@@ -7,32 +8,69 @@ $(document).ready(function(){
 
     //--------------data----------------
         function weather () {
-        var request = $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/29.4241,-98.4936");
-        request.done(function (data) {
-            console.log(data);
-            var date = function (input) {
-                return new Date(input * 1000);
-            };
+            var request = $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/29.4241,-98.4936");
+            request.done(function (data) {
+                console.log(data);
+                var date = function (input) {
+                    return new Date(input * 1000);
+                };
 
-            var i = 0;
-            do {
-            var dailyWeather = '';
-                dailyWeather += '<div class="card">';
-                dailyWeather += '<div class="card-title">' + "Todays Weather" + "</div>";
-                dailyWeather += '<div class="card-body"> <hr> <h1>' + "High" + " " + data.daily.data[i].temperatureHigh + "/" + "Low" + " " + data.daily.data[i].temperatureLow + "</h1> <p>" + data.daily.data[i].icon + "</p> <p>" + "Summary: " + data.daily.data[i].summary + "</p> <p>" + "Humidity: " + data.daily.data[i].humidity + "</p>";
-                // dailyWeather += '<span>' + date(data.daily.data[0].time) + '</span>';
-            $('#weatherDisplay').append(dailyWeather);
-            i++;
+                var i = 0;
+                do {
+                    var dailyWeather = '';
+                    dailyWeather += '<div class="card">';
+                    dailyWeather += '<div class="card-title">' + "Todays Weather" + "</div>";
+                    dailyWeather += '<div class="card-body"> <hr> ' +
+                        '<h1>' + "High" + " " + data.daily.data[i].temperatureHigh + "/" + "Low" + " " + data.daily.data[i].temperatureLow + "</h1> " +
+                        "<div class='w_icon'> </div> " +
+                        "<p>" + data.daily.data[i].icon + "</p> " +
+                        "<p>" + "Summary: " + data.daily.data[i].summary + "</p>" +
+                        " <p>" + "Humidity: " + data.daily.data[i].humidity + "</p>";
+                    // dailyWeather += '<span>' + date(data.daily.data[0].time) + '</span>';
+                    $('#weatherDisplay').append(dailyWeather);
+
+                    var weatherIcons = [
+                        {
+                            icon: "clear-day",
+                            img: "<img src='img/weatherIcons/clear-day.png' alt='icon'>"
+                        },
+                        {
+                            icon: "rain",
+                            img: "<img src='img/weatherIcons/rain.png' alt='icon'>"
+                        },
+                        {
+                            icon: "snow",
+                            img: "<img src='img/weatherIcons/snow.png' alt='icon'>"
+                        },
+                        {
+                            icon: "wind",
+                            img: "<img src='img/weatherIcons/wind.png' alt='icon'>"
+                        },
+                        {
+                            icon: "fog",
+                            img: "<img src='img/weatherIcons/fog.png' alt='icon'>"
+                        },
+                        {
+                            icon: "cloudy",
+                            img: "<img src='img/weatherIcons/fog.png' alt='icon'>"
+                        },
+                        {
+                            icon: "partly-cloudy-day",
+                            img: "<img src='img/weatherIcons/partly-cloudy-day.png' alt='icon'>"
+                        }
+                    ];
+
+                    weatherIcons.forEach(function (type) {
+                        if (data.daily.data[i].icon === type.icon) {
+                            $('.w_icon').html(type.img);
+                        }
+                    });
+                    i++;
                 } while (i <= 2);
-        });
-    }
-    weather();
 
-       var weatherIcons = [
-           {}, {}, {}, {}, {}, {}, {}, {}
-        ]
-
+            });
+        }
 //    ------------------------mapBox-----------------------------------
 
-
+weather();
 });
