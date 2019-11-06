1209cb9593fa0674c95de11b818fc0a8bab19248
andrewswint <andrewswint1976@gmail.com>
2019-11-06 11:25:33 -0600

    geocoding

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,20 +1,52 @@
 
 $(document).ready(function(){
-    console.log("Hello");
+    //    ------------------------mapBox-----------------------------------
+    var arr = [-98.4936, 29.4241];
+    var ll = mapboxgl.LngLat.convert(arr);
+
+
+    mapboxgl.accessToken = mapboxToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/dark-v9',
+        zoom: 10,
+        center: ll,
+        minZoom: 0
+    });
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat(ll)
+        .addTo(map);
+
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+    var lngLat = marker.getLngLat();
+    var request = $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + lngLat.lat + "," + lngLat.lng);
+
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        coordinates.style.display = 'block';
+        coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+        var newRequest = $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + lngLat.lat + "," + lngLat.lng);
+        weather(newRequest);
+    }
+
+    marker.on('dragend', onDragEnd);
     //-------------------------darkSky code-----------------------------------
-    //-------------icons-----------------
-
-
-
     //--------------data----------------
-        function weather () {
-            var request = $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/29.4241,-98.4936");
+        function weather (request) {
             request.done(function (data) {
                 console.log(data);
                 var date = function (input) {
                     return new Date(input * 1000);
                 };
-
+                $("#weatherDisplay").html("");
                 var i = 0;
                 do {
                     var dailyWeather = '';
@@ -23,7 +55,7 @@ $(document).ready(function(){
                     dailyWeather += '<div class="card-body"> <hr> ' +
                         '<h1>' + "High" + " " + data.daily.data[i].temperatureHigh + "/" + "Low" + " " + data.daily.data[i].temperatureLow + "</h1> " +
                         "<div class='w_icon'> </div> " +
-                        "<p>" + data.daily.data[i].icon + "</p> " +
+                        "<p>" + "</p> " +
                         "<p>" + "Summary: " + data.daily.data[i].summary + "</p>" +
                         " <p>" + "Humidity: " + data.daily.data[i].humidity + "</p>";
                     // dailyWeather += '<span>' + date(data.daily.data[0].time) + '</span>';
@@ -70,7 +102,8 @@ $(document).ready(function(){
 
             });
         }
-//    ------------------------mapBox-----------------------------------
+    weather(request);
 
-weather();
-});
+
+
+    });
